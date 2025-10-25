ff2a76bb79a90854af7ddf5ef392d91d5dee8676
Hari Ragavendra <ragavendra0604@gmail.com>
2025-10-25 09:20:43 +0530

    deployment-update

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,14 @@ app.use('/api/places', placeRoutes); // All places-related routes will start wit
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
